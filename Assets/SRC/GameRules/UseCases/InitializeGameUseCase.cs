9e23ddd8f97721e7e53dfdf62b982c1dd5ec1233
Lucas Rosales <yonk_cs@hotmail.com>
2023-08-24 18:44:30 +0200

    rename fileds in Initialize

--- Assets/SRC/GameRules/UseCases/InitializeGameUseCase.cs
+++ Assets/SRC/GameRules/UseCases/InitializeGameUseCase.cs
@@ -5,14 +5,14 @@ namespace GameRules
     public class InitializeGameUseCase
     {
         [Inject] private readonly ZoneFactory _zoneFactory;
-        [Inject] private readonly CardFactory _cardFactory2;
-        [Inject] private readonly ICardGenerator _cardFactory;
+        [Inject] private readonly CardFactory _cardFactory;
+        [Inject] private readonly ICardGenerator _cardGenerator;
 
         public void Execute()
         {
             _zoneFactory.CreateZones();
-            _cardFactory2.CreateCards();
-            _cardFactory.BuildCards();
+            _cardFactory.CreateCards();
+            _cardGenerator.BuildCards();
         }
     }
 }
