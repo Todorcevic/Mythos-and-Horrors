0da853e7144b10ee9c93a3f5e38b01ecc7218aa7
Lucas Rosales <yonk_cs@hotmail.com>
2024-01-08 14:23:34 +0100

    Pre unpack play area

--- Assets/SRC/GameView/Components/TokensGeneratorComponent.cs
+++ Assets/SRC/GameView/Components/TokensGeneratorComponent.cs
@@ -1,4 +1,5 @@
-﻿using Sirenix.OdinInspector;
+﻿using DG.Tweening;
+using Sirenix.OdinInspector;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
@@ -7,6 +8,7 @@ namespace MythsAndHorrors.GameView
 {
     public class TokensGeneratorComponent : MonoBehaviour
     {
+        [SerializeField, Required, ChildGameObjectsOnly] private Transform _showToken;
         [SerializeField, Required, ChildGameObjectsOnly] private List<TokenView> _resourceTokensView;
         [SerializeField, Required, ChildGameObjectsOnly] private List<TokenView> _hintsTokensView;
 
@@ -14,5 +16,17 @@ namespace MythsAndHorrors.GameView
         public List<TokenView> GetResourceTokens(int amount = 1) => _resourceTokensView.Take(amount).ToList();
 
         public List<TokenView> GetHintTokens(int amount = 1) => _hintsTokensView.Take(amount).ToList();
+
+        public void OnMouseEnter()
+        {
+            _showToken.DORotate(new Vector3(-45, 0, 0), ViewValues.FAST_TIME_ANIMATION);
+            _showToken.DOLocalMoveY(1, ViewValues.FAST_TIME_ANIMATION);
+            _showToken.DOLocalMoveZ(2, ViewValues.FAST_TIME_ANIMATION);
+        }
+
+        public void OnMouseExit()
+        {
+            _showToken.DOFullMove(_showToken.parent);
+        }
     }
 }
