26fa621dc870206bcc80a29a52c283a3d0c63476
Lucas Rosales <yonk_cs@hotmail.com>
2024-03-22 19:11:53 +0100

    MultiEffect test fixed

--- Assets/SRC/Tests.PlayMode/MultiEffectTests.cs
+++ Assets/SRC/Tests.PlayMode/MultiEffectTests.cs
@@ -1,63 +1,71 @@
-﻿//using MythosAndHorrors.GameRules;
-//using MythosAndHorrors.GameView;
-//using NUnit.Framework;
-//using System.Collections;
-//using System.Linq;
-//using System.Threading.Tasks;
-//using UnityEngine;
-//using UnityEngine.TestTools;
-//using Zenject;
+﻿using MythosAndHorrors.GameRules;
+using MythosAndHorrors.GameView;
+using NUnit.Framework;
+using System.Collections;
+using System.Linq;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.TestTools;
+using Zenject;
 
-//namespace MythosAndHorrors.PlayMode.Tests
-//{
-//    [TestFixture]
-//    public class MultiEffectTests : TestBase
-//    {
-//        [Inject] private readonly GameActionsProvider _gameActionsProvider;
-//        [Inject] private readonly InvestigatorsProvider _investigatorsProvider;
-//        [Inject] private readonly CardViewsManager _cardViewsManager;
-//        [Inject] private readonly ZoneViewsManager _zoneViewManager;
+namespace MythosAndHorrors.PlayMode.Tests
+{
+    [TestFixture]
+    public class MultiEffectTests : TestBase
+    {
+        [Inject] private readonly GameActionsProvider _gameActionsProvider;
+        [Inject] private readonly InvestigatorsProvider _investigatorsProvider;
+        [Inject] private readonly CardViewsManager _cardViewsManager;
+        [Inject] private readonly ZoneViewsManager _zoneViewManager;
 
-//        //protected override bool DEBUG_MODE => true;
+        //protected override bool DEBUG_MODE => true;
 
-//        /*******************************************************************/
-//        [UnityTest]
-//        public IEnumerator MultiEffect_Test()
-//        {
-//            InteractableGameAction interactableGameAction = new();
-//            Investigator investigator1 = _investigatorsProvider.First;
-//            Card card = investigator1.Cards[1];
-//            Card card2 = investigator1.Cards[2];
+        /*******************************************************************/
+        [UnityTest]
+        public IEnumerator MultiEffect_Test()
+        {
+            InteractableGameAction interactableGameAction = new();
+            Investigator investigator1 = _investigatorsProvider.First;
+            Card card = investigator1.Cards[1];
+            Card card2 = investigator1.Cards[2];
 
-//            interactableGameAction.CreateMainButton()
-//                     .SetDescription("Continue")
-//                     .SetLogic(() => Task.CompletedTask);
+            interactableGameAction.CreateMainButton()
+                     .SetDescription("Continue")
+                     .SetLogic(() => Task.CompletedTask);
 
-//            Effect effectToClick = interactableGameAction.Create()
-//                   .SetCard(card)
-//                   .SetInvestigator(investigator1)
-//                   .SetDescription("EffectOne")
-//                   .SetLogic(() => _gameActionsProvider.Create(new MoveCardsGameAction(card, investigator1.DangerZone)));
+            Effect effectToClick = interactableGameAction.Create()
+                   .SetCard(card)
+                   .SetInvestigator(investigator1)
+                   .SetDescription("EffectOne")
+                   .SetLogic(() => _gameActionsProvider.Create(new MoveCardsGameAction(card, investigator1.DangerZone)));
 
-//            interactableGameAction.Create()
-//                .SetCard(card)
-//                .SetInvestigator(investigator1)
-//                .SetDescription("EffectTwo")
-//                .SetLogic(() => _gameActionsProvider.Create(new MoveCardsGameAction(card, investigator1.HandZone)));
+            interactableGameAction.Create()
+                .SetCard(card)
+                .SetInvestigator(investigator1)
+                .SetDescription("EffectTwo")
+                .SetLogic(() => _gameActionsProvider.Create(new MoveCardsGameAction(card, investigator1.HandZone)));
 
-//            interactableGameAction.Create()
-//                .SetCard(card2)
-//                .SetInvestigator(investigator1)
-//                .SetDescription("EffectOne")
-//                .SetLogic(() => _gameActionsProvider.Create(new MoveCardsGameAction(card2, investigator1.DangerZone)));
+            interactableGameAction.Create()
+                .SetCard(card2)
+                .SetInvestigator(investigator1)
+                .SetDescription("EffectOne")
+                .SetLogic(() => _gameActionsProvider.Create(new MoveCardsGameAction(card2, investigator1.DangerZone)));
 
-//            yield return _gameActionsProvider.Create(new MoveCardsGameAction(investigator1.Cards.Take(5).ToList(), investigator1.HandZone)).AsCoroutine();
-//            if (!DEBUG_MODE) WaitToClick(card).AsTask();
-//            if (!DEBUG_MODE) WaitToCloneClick(effectToClick).AsTask();
-//            yield return _gameActionsProvider.Create(interactableGameAction).AsCoroutine();
+            yield return _gameActionsProvider.Create(new MoveCardsGameAction(investigator1.Cards.Take(5).ToList(), investigator1.HandZone)).AsCoroutine();
+            if (!DEBUG_MODE) WaitToClick2(card).AsTask();
+            yield return _gameActionsProvider.Create(interactableGameAction).AsCoroutine();
 
-//            if (DEBUG_MODE) yield return new WaitForSeconds(230);
-//            Assert.That(investigator1.DangerZone.TopCard, Is.EqualTo(card));
-//        }
-//    }
-//}
+            if (DEBUG_MODE) yield return new WaitForSeconds(230);
+            Assert.That(investigator1.DangerZone.TopCard, Is.EqualTo(card));
+        }
+
+        private IEnumerator WaitToClick2(Card card)
+        {
+            CardSensorController cardSensor = _cardViewsManager.GetCardView(card).GetComponentInChildren<CardSensorController>();
+            yield return new WaitUntil(() => cardSensor.IsClickable);
+            cardSensor.OnMouseUpAsButton();
+            yield return new WaitUntil(() => cardSensor.IsClickable && _zoneViewManager.SelectorZone.GetComponentsInChildren<CardView>().Length > 0);
+            cardSensor.OnMouseUpAsButton();
+        }
+    }
+}
