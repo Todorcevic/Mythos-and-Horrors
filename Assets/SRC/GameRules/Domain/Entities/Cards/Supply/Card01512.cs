fb77edf81925d429041b985d39eb889a4502b82f
Lucas Rosales <yonk_cs@hotmail.com>
2024-05-29 15:11:34 +0200

    Card01512 done

--- Assets/SRC/GameRules/Domain/Entities/Cards/Supply/Card01512.cs
+++ Assets/SRC/GameRules/Domain/Entities/Cards/Supply/Card01512.cs
@@ -1,10 +1,37 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Threading.Tasks;
+using Zenject;
 
 namespace MythosAndHorrors.GameRules
 {
     public class Card01512 : CardSupply
     {
+        [Inject] private readonly GameActionsProvider _gameActionsProvider;
+
         public override IEnumerable<Tag> Tags => new[] { Tag.Item, Tag.Relic };
 
+        /*******************************************************************/
+        [Inject]
+        [SuppressMessage("CodeQuality", "IDE0051:Remove unused private members", Justification = "Injected by Zenject")]
+        private void Init()
+        {
+            CreateReaction<PlayFromHandGameAction>(DrawCondition, DrawLogic, false, isOptative: true);
+        }
+
+        /*******************************************************************/
+        private async Task DrawLogic(PlayFromHandGameAction action)
+        {
+            await _gameActionsProvider.Create(new DrawAidGameAction(ControlOwner));
+        }
+
+        private bool DrawCondition(PlayFromHandGameAction playFromHandGameAction)
+        {
+            if (!IsInPlay) return false;
+            if (playFromHandGameAction.Investigator != ControlOwner) return false;
+            if (!((Card)playFromHandGameAction.PlayableFromHandCard).HasThisTag(Tag.Spell)) return false;
+            return true;
+        }
     }
 }
---
+++ Assets/SRC/Tests.PlayMode/Tests.PlayModeCORE1/Cards/CardSupply01512Tests.cs
@@ -0,0 +1,37 @@
+﻿using MythosAndHorrors.GameRules;
+using NUnit.Framework;
+using System.Collections;
+using UnityEngine.TestTools;
+using MythosAndHorrors.PlayMode.Tests;
+using System.Threading.Tasks;
+
+namespace MythosAndHorrors.PlayModeCORE1.Tests
+{
+    public class CardSupply01512Tests : TestCORE1Preparation
+    {
+        //protected override TestsType TestsType => TestsType.Debug;
+
+        [UnityTest]
+        public IEnumerator DrawWhenPlayASpell()
+        {
+            Investigator investigator = _investigatorsProvider.First;
+            CardSupply hiperborea = _cardsProvider.GetCard<Card01512>();
+            Card01560 spell = _cardsProvider.GetCard<Card01560>();
+            yield return PlaceOnlyScene();
+            yield return PlayThisInvestigator(investigator);
+            Card card = investigator.CardAidToDraw;
+            yield return _gameActionsProvider.Create(new MoveCardsGameAction(hiperborea, investigator.AidZone)).AsCoroutine();
+            yield return _gameActionsProvider.Create(new MoveCardsGameAction(spell, investigator.HandZone)).AsCoroutine();
+
+            Assert.That(card.CurrentZone, Is.EqualTo(investigator.DeckZone));
+            Task<PlayInvestigatorGameAction> gameActionTask = _gameActionsProvider.Create(new PlayInvestigatorGameAction(investigator));
+            yield return ClickedIn(spell);
+            yield return ClickedIn(hiperborea);
+            yield return ClickedMainButton();
+            yield return gameActionTask.AsCoroutine();
+
+            Assert.That(card.CurrentZone, Is.Not.EqualTo(investigator.DeckZone));
+        }
+
+    }
+}
