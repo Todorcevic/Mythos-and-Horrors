d65cae07b34f540422b8aa0782df1a80fc398b29
Lucas Rosales <yonk_cs@hotmail.com>
2024-05-29 15:36:20 +0200

    Card01513 done

--- Assets/SRC/GameRules/Domain/Entities/Cards/Condition/Card01513.cs
+++ Assets/SRC/GameRules/Domain/Entities/Cards/Condition/Card01513.cs
@@ -1,15 +1,44 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Threading.Tasks;
+using Zenject;
 
 namespace MythosAndHorrors.GameRules
 {
     public class Card01513 : CardCondition
     {
+        [Inject] private readonly GameActionsProvider _gameActionsProvider;
+        [Inject] private readonly ChaptersProvider _chaptersProvider;
+
         public override IEnumerable<Tag> Tags => new[] { Tag.Spell, Tag.Weakness };
 
-        public override Task ExecuteConditionEffect()
+        /*******************************************************************/
+        [Inject]
+        [SuppressMessage("CodeQuality", "IDE0051:Remove unused private members", Justification = "Injected by Zenject")]
+        private void Init()
         {
-            throw new System.NotImplementedException();
+            CreateReaction<PlayInvestigatorGameAction>(Condition, Logic, false);
+        }
+
+        /*******************************************************************/
+        private async Task Logic(PlayInvestigatorGameAction playInvestigatorGameAction)
+        {
+            await _gameActionsProvider.Create(new HarmToInvestigatorGameAction(ControlOwner, amountFear: 2, fromCard: this));
+        }
+
+        private bool Condition(PlayInvestigatorGameAction playInvestigatorGameAction)
+        {
+            if (CurrentZone != playInvestigatorGameAction.ActiveInvestigator.HandZone) return false;
+            return true;
+        }
+
+        /*******************************************************************/
+
+        public override async Task ExecuteConditionEffect()
+        {
+            await _gameActionsProvider.Create(new DecrementStatGameAction(_chaptersProvider.CurrentScene.CurrentPlot?.Eldritch, 1));
+            await _gameActionsProvider.Create(new CheckEldritchsPlotGameAction());
         }
     }
 }
---
+++ Assets/SRC/Tests.PlayMode/Tests.PlayModeCORE1/Cards/CardCondition01513Tests.cs
@@ -0,0 +1,45 @@
+﻿using MythosAndHorrors.GameRules;
+using MythosAndHorrors.PlayMode.Tests;
+using NUnit.Framework;
+using System.Collections;
+using System.Threading.Tasks;
+using UnityEngine.TestTools;
+
+namespace MythosAndHorrors.PlayModeCORE1.Tests
+{
+    public class CardCondition01513Tests : TestCORE1Preparation
+    {
+        [UnityTest]
+        public IEnumerator PlaceEldritch()
+        {
+            Investigator investigator = _investigatorsProvider.First;
+            Card01513 darkMemory = _cardsProvider.GetCard<Card01513>();
+            yield return PlaceOnlyScene();
+            yield return PlayThisInvestigator(investigator);
+
+            yield return _gameActionsProvider.Create(new MoveCardsGameAction(darkMemory, investigator.HandZone)).AsCoroutine();
+
+            Task<PlayInvestigatorGameAction> gameActionTask = _gameActionsProvider.Create(new PlayInvestigatorGameAction(investigator));
+            yield return ClickedIn(darkMemory);
+            yield return ClickedMainButton();
+
+            Assert.That(SceneCORE1.CurrentPlot.AmountOfEldritch, Is.EqualTo(1));
+        }
+
+        [UnityTest]
+        public IEnumerator TakeFear()
+        {
+            Investigator investigator = _investigatorsProvider.First;
+            Card01513 darkMemory = _cardsProvider.GetCard<Card01513>();
+            yield return PlaceOnlyScene();
+            yield return PlayThisInvestigator(investigator);
+
+            yield return _gameActionsProvider.Create(new MoveCardsGameAction(darkMemory, investigator.HandZone)).AsCoroutine();
+
+            Task<PlayInvestigatorGameAction> gameActionTask = _gameActionsProvider.Create(new PlayInvestigatorGameAction(investigator));
+            yield return ClickedMainButton();
+
+            Assert.That(investigator.FearRecived, Is.EqualTo(2));
+        }
+    }
+}
