88f286b0b73938a8d0d2213a0028e957264e6705
Lucas Rosales <yonk_cs@hotmail.com>
2024-06-12 21:12:39 +0200

    Card01579 done

--- Assets/SRC/GameRules/Domain/Entities/Cards/Condition/Card01579.cs
+++ Assets/SRC/GameRules/Domain/Entities/Cards/Condition/Card01579.cs
@@ -1,20 +1,32 @@
 ﻿using System.Collections.Generic;
 using System.Threading.Tasks;
+using Zenject;
 
 namespace MythosAndHorrors.GameRules
 {
-    public class Card01579 : CardCondition
+    public class Card01579 : CardConditionTrigged
     {
+        [Inject] private readonly GameActionsProvider _gameActionsProvider;
+
         public override IEnumerable<Tag> Tags => new[] { Tag.Fortune };
+        protected override bool IsFast => true;
+        protected override bool FastReactionAtStart => false;
 
-        protected override bool IsFast => false;
-
+        /*******************************************************************/
         protected override async Task ExecuteConditionEffect(Investigator investigator)
         {
-            await Task.CompletedTask;
+            await _gameActionsProvider.Create(new GainHintGameAction(investigator, investigator.CurrentPlace.Hints, 2));
         }
 
-        protected override bool CanPlayFromHandSpecific(GameAction gameAction) => true;
+        protected override bool CanPlayFromHandSpecific(GameAction gameAction)
+        {
+            if (gameAction is not InvestigateGameAction investigateGameAction) return false;
+            if (investigateGameAction.ActiveInvestigator != ControlOwner) return false;
+            if (investigateGameAction.IsSuccessful ?? true) return false;
+            if (investigateGameAction.CurrentTotalChallengeValue < -2) return false;
+            if (ControlOwner.CurrentPlace.Hints.Value < 1) return false;
+            return true;
+        }
 
     }
 }
---
+++ Assets/SRC/Tests.PlayMode/Tests.PlayModeCORE2/Cards/CardCondition01579Tests.cs
@@ -0,0 +1,36 @@
+﻿using MythosAndHorrors.GameRules;
+using NUnit.Framework;
+using System.Collections;
+using UnityEngine.TestTools;
+using MythosAndHorrors.PlayMode.Tests;
+using System.Threading.Tasks;
+
+namespace MythosAndHorrors.PlayModeCORE2.Tests
+{
+    public class CardCondition01579Tests : TestCORE2Preparation
+    {
+        //protected override TestsType TestsType => TestsType.Debug;
+
+        [UnityTest]
+        public IEnumerator DiscoverClues()
+        {
+            Investigator investigator = _investigatorsProvider.Fourth;
+            _ = MustBeRevealedThisToken(ChallengeTokenType.Value_4);
+            yield return PlaceOnlyScene();
+            yield return PlayThisInvestigator(investigator);
+            //yield return BuilCard("01568", investigator);
+            Card01579 conditionCard = _cardsProvider.GetCard<Card01579>();
+            yield return _gameActionsProvider.Create(new MoveCardsGameAction(conditionCard, investigator.HandZone)).AsCoroutine();
+
+            Task gameActionTask = _gameActionsProvider.Create(new PlayInvestigatorGameAction(investigator));
+            yield return ClickedIn(investigator.CurrentPlace);
+            yield return ClickedMainButton();
+            yield return ClickedMainButton();
+            yield return ClickedIn(conditionCard);
+            yield return ClickedMainButton();
+            yield return gameActionTask.AsCoroutine();
+
+            Assert.That(investigator.Hints.Value, Is.EqualTo(2));
+        }
+    }
+}
