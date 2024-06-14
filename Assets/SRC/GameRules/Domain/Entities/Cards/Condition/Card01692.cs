ff1c2c2a599245a5b09b5979cef53faeb97c7400
Lucas Rosales <yonk_cs@hotmail.com>
2024-06-14 09:28:51 +0200

    Card01692 done

--- Assets/SRC/GameRules/Domain/Entities/Cards/Condition/Card01692.cs
+++ Assets/SRC/GameRules/Domain/Entities/Cards/Condition/Card01692.cs
@@ -1,20 +1,32 @@
 ﻿using System.Collections.Generic;
 using System.Threading.Tasks;
+using Zenject;
 
 namespace MythosAndHorrors.GameRules
 {
-    public class Card01692 : CardConditionPlayFromHand
+    public class Card01692 : CardConditionTrigged
     {
+        [Inject] private readonly GameActionsProvider _gameActionsProvider;
+        [Inject] private readonly ChaptersProvider _chaptersProvider;
+
         public override IEnumerable<Tag> Tags => new[] { Tag.Fortune, Tag.Blessed };
+        protected override bool IsFast => true;
+        protected override bool FastReactionAtStart => false;
 
-        protected override bool IsFast => false;
-
-        protected override Task ExecuteConditionEffect(Investigator investigator)
+        /*******************************************************************/
+        protected override async Task ExecuteConditionEffect(GameAction gameAction, Investigator investigator)
         {
-            throw new System.NotImplementedException();
+            if (gameAction is not RevealChallengeTokenGameAction revealChallengeTokenGameAction) return;
+            await _gameActionsProvider.Create(new RestoreChallengeTokenGameAction(revealChallengeTokenGameAction.ChallengeTokenRevealed));
+            await _gameActionsProvider.Create(new RevealChallengeTokenGameAction(_chaptersProvider.CurrentScene.StarToken, investigator));
         }
 
-        protected override bool CanPlayFromHandSpecific(GameAction gameAction) => true;
-
+        protected override bool CanPlayFromHandSpecific(GameAction gameAction)
+        {
+            if (gameAction is not RevealChallengeTokenGameAction revealChallengeTokenGameAction) return false;
+            if (revealChallengeTokenGameAction.Investigator != ControlOwner) return false;
+            if (revealChallengeTokenGameAction.ChallengeTokenRevealed.TokenType != ChallengeTokenType.Fail) return false;
+            return true;
+        }
     }
 }
---
+++ Assets/SRC/Tests.PlayMode/Tests.PlayModeCORE2/Cards/CardCondition01692Tests.cs
@@ -0,0 +1,35 @@
+﻿using MythosAndHorrors.GameRules;
+using NUnit.Framework;
+using System.Collections;
+using UnityEngine.TestTools;
+using MythosAndHorrors.PlayMode.Tests;
+using System.Threading.Tasks;
+
+namespace MythosAndHorrors.PlayModeCORE2.Tests
+{
+    public class CardCondition01692Tests : TestCORE2Preparation
+    {
+        //protected override TestsType TestsType => TestsType.Debug;
+
+        [UnityTest]
+        public IEnumerator SwapToken()
+        {
+            Investigator investigator = _investigatorsProvider.Second;
+            _ = MustBeRevealedThisToken(ChallengeTokenType.Fail);
+            yield return PlaceOnlyScene();
+            yield return PlayThisInvestigator(investigator);
+            yield return BuildCard("01692", investigator);
+            Card01692 conditionCard = _cardsProvider.GetCard<Card01692>();
+            yield return _gameActionsProvider.Create(new MoveCardsGameAction(conditionCard, investigator.HandZone)).AsCoroutine();
+
+            Task gameActionTask = _gameActionsProvider.Create(new PlayInvestigatorGameAction(investigator));
+            yield return ClickedIn(investigator.CurrentPlace);
+            yield return ClickedMainButton();
+            yield return ClickedIn(conditionCard);
+            yield return ClickedMainButton();
+            yield return gameActionTask.AsCoroutine();
+
+            Assert.That(investigator.Hints.Value, Is.EqualTo(1));
+        }
+    }
+}
