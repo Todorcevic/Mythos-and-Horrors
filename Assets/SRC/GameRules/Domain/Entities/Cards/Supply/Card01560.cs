6e0424d5a8bf794417fa7e1d29bbd06835f99709
Lucas Rosales <yonk_cs@hotmail.com>
2024-06-23 11:13:10 +0200

    Card 01560 done

--- Assets/SRC/GameRules/Domain/Entities/Cards/Supply/Card01560.cs
+++ Assets/SRC/GameRules/Domain/Entities/Cards/Supply/Card01560.cs
@@ -1,10 +1,49 @@
 ﻿using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Threading.Tasks;
+using Zenject;
 
 namespace MythosAndHorrors.GameRules
 {
-    public class Card01560 : CardSupply
+    public class Card01560 : CardWeapon, IChargeable
     {
-        public override IEnumerable<Tag> Tags => new[] { Tag.Spell };
+        private AttackCreatureGameAction _attackCreatureGameAction;
+        [Inject] private readonly GameActionsProvider _gameActionsProvider;
 
+        public override IEnumerable<Tag> Tags => new[] { Tag.Spell };
+        public Stat AmountCharges { get; private set; }
+
+        /*******************************************************************/
+        [Inject]
+        [SuppressMessage("CodeQuality", "IDE0051:Remove unused private members", Justification = "Injected by Zenject")]
+        private void Init()
+        {
+            AmountCharges = CreateStat(4);
+            CreateForceReaction<ResolveChallengeGameAction>(Condition, Logic, GameActionTime.After);
+        }
+
+        /*******************************************************************/
+        private async Task Logic(ResolveChallengeGameAction resolveChallengeGameAction)
+        {
+            await _gameActionsProvider.Create(new HarmToInvestigatorGameAction(ControlOwner, this, amountFear: 1));
+        }
+
+        private bool Condition(ResolveChallengeGameAction resolveChallengeGameAction)
+        {
+            if (resolveChallengeGameAction.ChallengePhaseGameAction != _attackCreatureGameAction) return false;
+            if (resolveChallengeGameAction.ChallengePhaseGameAction.ResultChallenge.TokensRevealed
+                .All(token => ((int)token.TokenType) < 10 || ((int)token.TokenType) > 50)) return false;
+            return true;
+        }
+
+        /*******************************************************************/
+        protected override async Task ExtraAttackEnemyLogic(AttackCreatureGameAction attackCreatureGameAction)
+        {
+            attackCreatureGameAction.ChangeStat(ControlOwner.Power);
+            await _gameActionsProvider.Create(new DecrementStatGameAction(AmountCharges, 1));
+            await _gameActionsProvider.Create(new IncrementStatGameAction(attackCreatureGameAction.AmountDamage, 1));
+            _attackCreatureGameAction = attackCreatureGameAction;
+        }
     }
 }
--- Assets/SRC/Tests.PlayMode/Tests.PlayModeCORE1/Cards/CardSupply01560Tests.cs
+++ Assets/SRC/Tests.PlayMode/Tests.PlayModeCORE1/Cards/CardSupply01560Tests.cs
@@ -13,22 +13,79 @@ namespace MythosAndHorrors.PlayModeCORE1.Tests
         //protected override TestsType TestsType => TestsType.Debug;
 
         [UnityTest]
-        public IEnumerator TakeResources()
+        public IEnumerator SucessAttack()
         {
-            Assert.That(false, "Not Implemented");
             Investigator investigator = _investigatorsProvider.First;
-            Card01560 cardSupply = _cardsProvider.GetCard<Card01560>();
+            _ = MustBeRevealedThisToken(ChallengeTokenType.Value1);
             yield return PlaceOnlyScene();
             yield return PlayThisInvestigator(investigator);
+            Card01560 weaponCard = _cardsProvider.GetCard<Card01560>();
+            yield return _gameActionsProvider.Create(new MoveCardsGameAction(weaponCard, investigator.AidZone)).AsCoroutine();
+            yield return _gameActionsProvider.Create(new MoveCardsGameAction(SceneCORE1.GhoulSecuaz, investigator.DangerZone)).AsCoroutine();
+            yield return _gameActionsProvider.Create(new MoveCardsGameAction(SceneCORE1.GhoulVoraz, investigator.DangerZone)).AsCoroutine();
 
-            yield return _gameActionsProvider.Create(new MoveCardsGameAction(cardSupply, investigator.HandZone)).AsCoroutine();
+            Assert.That(weaponCard.AmountCharges.Value, Is.EqualTo(4));
 
-            Task gameActionTask = _gameActionsProvider.Create(new PlayInvestigatorGameAction(investigator));
-            yield return ClickedIn(cardSupply);
+            Task<PlayInvestigatorGameAction> taskGameAction = _gameActionsProvider.Create(new PlayInvestigatorGameAction(investigator));
+            yield return ClickedIn(weaponCard);
+            yield return ClickedIn(SceneCORE1.GhoulVoraz);
             yield return ClickedMainButton();
-            yield return gameActionTask.AsCoroutine();
+            yield return ClickedMainButton();
+            yield return taskGameAction.AsCoroutine();
 
-            Assert.That(investigator.Resources.Value, Is.EqualTo(8));
+            Assert.That(SceneCORE1.GhoulVoraz.DamageRecived.Value, Is.EqualTo(2));
+            Assert.That(weaponCard.AmountCharges.Value, Is.EqualTo(3));
+        }
+
+        [UnityTest]
+        public IEnumerator FailAttack()
+        {
+            Investigator investigator = _investigatorsProvider.First;
+            _ = MustBeRevealedThisToken(ChallengeTokenType.Value_1);
+            yield return PlaceOnlyScene();
+            yield return PlayThisInvestigator(investigator);
+            Card01560 weaponCard = _cardsProvider.GetCard<Card01560>();
+            yield return _gameActionsProvider.Create(new MoveCardsGameAction(weaponCard, investigator.AidZone)).AsCoroutine();
+            yield return _gameActionsProvider.Create(new MoveCardsGameAction(SceneCORE1.GhoulSecuaz, investigator.DangerZone)).AsCoroutine();
+            yield return _gameActionsProvider.Create(new MoveCardsGameAction(SceneCORE1.GhoulVoraz, investigator.DangerZone)).AsCoroutine();
+
+            Assert.That(weaponCard.AmountCharges.Value, Is.EqualTo(4));
+
+            Task<PlayInvestigatorGameAction> taskGameAction = _gameActionsProvider.Create(new PlayInvestigatorGameAction(investigator));
+            yield return ClickedIn(weaponCard);
+            yield return ClickedIn(SceneCORE1.GhoulVoraz);
+            yield return ClickedMainButton();
+            yield return ClickedMainButton();
+            yield return taskGameAction.AsCoroutine();
+
+            Assert.That(SceneCORE1.GhoulVoraz.DamageRecived.Value, Is.EqualTo(0));
+            Assert.That(weaponCard.AmountCharges.Value, Is.EqualTo(3));
+        }
+
+        [UnityTest]
+        public IEnumerator SpecialTokenRevealed()
+        {
+            Investigator investigator = _investigatorsProvider.First;
+            _ = MustBeRevealedThisToken(ChallengeTokenType.Fail);
+            yield return PlaceOnlyScene();
+            yield return PlayThisInvestigator(investigator);
+            Card01560 weaponCard = _cardsProvider.GetCard<Card01560>();
+            yield return _gameActionsProvider.Create(new MoveCardsGameAction(weaponCard, investigator.AidZone)).AsCoroutine();
+            yield return _gameActionsProvider.Create(new MoveCardsGameAction(SceneCORE1.GhoulSecuaz, investigator.DangerZone)).AsCoroutine();
+            yield return _gameActionsProvider.Create(new MoveCardsGameAction(SceneCORE1.GhoulVoraz, investigator.DangerZone)).AsCoroutine();
+
+            Assert.That(weaponCard.AmountCharges.Value, Is.EqualTo(4));
+
+            Task<PlayInvestigatorGameAction> taskGameAction = _gameActionsProvider.Create(new PlayInvestigatorGameAction(investigator));
+            yield return ClickedIn(weaponCard);
+            yield return ClickedIn(SceneCORE1.GhoulVoraz);
+            yield return ClickedMainButton();
+            yield return ClickedMainButton();
+            yield return taskGameAction.AsCoroutine();
+
+            Assert.That(SceneCORE1.GhoulVoraz.DamageRecived.Value, Is.EqualTo(0));
+            Assert.That(investigator.FearRecived.Value, Is.EqualTo(1));
+            Assert.That(weaponCard.AmountCharges.Value, Is.EqualTo(3));
         }
     }
 }
