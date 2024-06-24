654aaa1623797a18f1ad0595c868b18316b50262
Lucas Rosales <yonk_cs@hotmail.com>
2024-06-24 10:51:59 +0200

    Card01518 done

--- Assets/SRC/Tests.PlayMode/Tests.PlayModeCORE1/Cards/CardSupply01518Tests.cs
+++ Assets/SRC/Tests.PlayMode/Tests.PlayModeCORE1/Cards/CardSupply01518Tests.cs
@@ -13,22 +13,27 @@ namespace MythosAndHorrors.PlayModeCORE1.Tests
         //protected override TestsType TestsType => TestsType.Debug;
 
         [UnityTest]
-        public IEnumerator TakeResources()
+        public IEnumerator DoDamageAndDiscard()
         {
-            Assert.That(false, "Not Implemented");
-            Investigator investigator = _investigatorsProvider.First;
+            Investigator investigator = _investigatorsProvider.Second;
             Card01518 cardSupply = _cardsProvider.GetCard<Card01518>();
+            CardCreature creature = SceneCORE1.GhoulVoraz;
             yield return PlaceOnlyScene();
             yield return PlayThisInvestigator(investigator);
 
-            yield return _gameActionsProvider.Create(new MoveCardsGameAction(cardSupply, investigator.HandZone)).AsCoroutine();
+            yield return _gameActionsProvider.Create(new MoveCardsGameAction(cardSupply, investigator.AidZone)).AsCoroutine();
+            yield return _gameActionsProvider.Create(new MoveCardsGameAction(creature, investigator.DangerZone)).AsCoroutine();
+
+            Assert.That(investigator.Strength.Value, Is.EqualTo(3));
 
             Task gameActionTask = _gameActionsProvider.Create(new PlayInvestigatorGameAction(investigator));
             yield return ClickedIn(cardSupply);
+            yield return ClickedIn(creature);
             yield return ClickedMainButton();
             yield return gameActionTask.AsCoroutine();
 
-            Assert.That(investigator.Resources.Value, Is.EqualTo(8));
+            Assert.That(creature.DamageRecived.Value, Is.EqualTo(1));
+            Assert.That(cardSupply.CurrentZone, Is.EqualTo(investigator.DiscardZone));
         }
     }
 }
