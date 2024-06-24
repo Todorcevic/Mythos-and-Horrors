898b9c32c61e3fbb6c5206078c2309b7b56d0bbb
Lucas Rosales <yonk_cs@hotmail.com>
2024-06-24 11:31:41 +0200

    Card 01527 done

--- Assets/SRC/GameRules/Domain/Entities/Cards/Supply/Card01527.cs
+++ Assets/SRC/GameRules/Domain/Entities/Cards/Supply/Card01527.cs
@@ -1,10 +1,56 @@
-﻿using System.Collections.Generic;
+﻿using ModestTree;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Threading.Tasks;
+using Zenject;
 
 namespace MythosAndHorrors.GameRules
 {
     public class Card01527 : CardSupply
     {
+        [Inject] private readonly GameActionsProvider _gameActionsProvider;
+
         public override IEnumerable<Tag> Tags => new[] { Tag.Item };
 
+        /*******************************************************************/
+        [Inject]
+        [SuppressMessage("CodeQuality", "IDE0051:Remove unused private members", Justification = "Injected by Zenject")]
+        private void Init()
+        {
+            CreateBuff(CardsToBuff, GainPowerActivationLogic, GainPowerDeactivationLogic);
+            CreateFastActivation(Logic, Condition, PlayActionType.Activate);
+        }
+
+        /*******************************************************************/
+        private bool Condition(Investigator investigator)
+        {
+            if (!IsInPlay) return false;
+            return true;
+        }
+
+        private async Task Logic(Investigator investigator)
+        {
+            await _gameActionsProvider.Create(new DiscardGameAction(this));
+            await _gameActionsProvider.Create(new IncrementStatGameAction(investigator.CurrentTurns, 2));
+        }
+
+        /*******************************************************************/
+        private async Task GainPowerDeactivationLogic(IEnumerable<Card> cardsToBuff)
+        {
+            CardInvestigator cardInvestigator = cardsToBuff.OfType<CardInvestigator>().First();
+            await _gameActionsProvider.Create(new DecrementStatGameAction(cardInvestigator.Strength, 1));
+        }
+
+        private async Task GainPowerActivationLogic(IEnumerable<Card> cardsToBuff)
+        {
+            CardInvestigator cardInvestigator = cardsToBuff.OfType<CardInvestigator>().First();
+            await _gameActionsProvider.Create(new IncrementStatGameAction(cardInvestigator.Strength, 1));
+        }
+
+        private IEnumerable<Card> CardsToBuff() => IsInPlay ? new[] { ControlOwner.InvestigatorCard } : new Card[0];
+
+        /*******************************************************************/
     }
 }
--- Assets/SRC/Tests.PlayMode/Tests.PlayModeCORE1/Cards/CardSupply01527Tests.cs
+++ Assets/SRC/Tests.PlayMode/Tests.PlayModeCORE1/Cards/CardSupply01527Tests.cs
@@ -13,22 +13,28 @@ namespace MythosAndHorrors.PlayModeCORE1.Tests
         //protected override TestsType TestsType => TestsType.Debug;
 
         [UnityTest]
-        public IEnumerator TakeResources()
+        public IEnumerator ExtraTurns()
         {
-            Assert.That(false, "Not Implemented");
             Investigator investigator = _investigatorsProvider.First;
+            Investigator investigator2 = _investigatorsProvider.Second;
+            yield return BuildCard("01527", investigator);
             Card01527 cardSupply = _cardsProvider.GetCard<Card01527>();
             yield return PlaceOnlyScene();
             yield return PlayThisInvestigator(investigator);
+            yield return PlayThisInvestigator(investigator2);
+            yield return _gameActionsProvider.Create(new MoveCardsGameAction(cardSupply, investigator.AidZone)).AsCoroutine();
 
-            yield return _gameActionsProvider.Create(new MoveCardsGameAction(cardSupply, investigator.HandZone)).AsCoroutine();
-
-            Task gameActionTask = _gameActionsProvider.Create(new PlayInvestigatorGameAction(investigator));
+            Task gameActionTask = _gameActionsProvider.Create(new PlayInvestigatorGameAction(investigator2));
             yield return ClickedIn(cardSupply);
+            yield return ClickedTokenButton();
+            yield return ClickedTokenButton();
+            yield return ClickedTokenButton();
+            yield return ClickedTokenButton();
+            yield return ClickedTokenButton();
             yield return ClickedMainButton();
             yield return gameActionTask.AsCoroutine();
 
-            Assert.That(investigator.Resources.Value, Is.EqualTo(8));
+            Assert.That(investigator2.Resources.Value, Is.EqualTo(10));
         }
     }
 }
