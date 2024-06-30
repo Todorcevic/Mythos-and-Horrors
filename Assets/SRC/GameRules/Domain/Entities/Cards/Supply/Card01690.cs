9778b0e276ddfa1d44dacde580f345087b6dbbf7
Lucas Rosales <yonk_cs@hotmail.com>
2024-06-30 12:13:26 +0200

    Card 01690 done

--- Assets/SRC/GameRules/Domain/Entities/Cards/Supply/Card01690.cs
+++ Assets/SRC/GameRules/Domain/Entities/Cards/Supply/Card01690.cs
@@ -1,10 +1,95 @@
 ﻿using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Threading.Tasks;
+using Zenject;
 
 namespace MythosAndHorrors.GameRules
 {
     public class Card01690 : CardSupply
     {
+        [Inject] private readonly GameActionsProvider _gameActionsProvider;
+        [Inject] private readonly InvestigatorsProvider _investigatorsProvider;
+        [Inject] private readonly ChaptersProvider _chaptersProvider;
         public override IEnumerable<Tag> Tags => new[] { Tag.Spell };
 
+        public Charge Charge { get; private set; }
+
+        /*******************************************************************/
+        [Inject]
+        [SuppressMessage("CodeQuality", "IDE0051:Remove unused private members", Justification = "Injected by Zenject")]
+        private void Init()
+        {
+            Charge = new Charge(3, ChargeType.MagicCharge);
+            CreateFastActivation(Logic, Condition, PlayActionType.Activate);
+        }
+
+        /*******************************************************************/
+        private bool Condition(Investigator investigator)
+        {
+            if (!IsInPlay) return false;
+            if (investigator != ControlOwner) return false;
+            if (Charge.IsEmpty) return false;
+            return true;
+        }
+
+        private async Task Logic(Investigator investigator)
+        {
+            InteractableGameAction interactableGameAction = new(canBackToThisInteractable: true, mustShowInCenter: true, "Choose deck");
+            interactableGameAction.CreateCancelMainButton();
+            interactableGameAction.CreateEffect(_chaptersProvider.CurrentScene.CardDangerToDraw, new Stat(0, false), SelectDangerDeck, PlayActionType.Choose, investigator);
+
+            foreach (Investigator inv in _investigatorsProvider.AllInvestigatorsInPlay)
+            {
+                interactableGameAction.CreateEffect(inv.InvestigatorCard, new Stat(0, false), SelectDeck, PlayActionType.Choose, investigator);
+
+                async Task SelectDeck()
+                {
+                    IEnumerable<Card> cards = inv.DeckZone.Cards.TakeLast(3);
+                    await _gameActionsProvider.Create(new ShowCardsGameAction(cards));
+                    await SortCards(cards, inv);
+                }
+            }
+
+            await _gameActionsProvider.Create(new DecrementStatGameAction(Charge.Amount, 1));
+            await _gameActionsProvider.Create(interactableGameAction);
+
+            /*******************************************************************/
+            async Task SelectDangerDeck()
+            {
+                IEnumerable<Card> cards = _chaptersProvider.CurrentScene.DangerDeckZone.Cards.TakeLast(3);
+                await _gameActionsProvider.Create(new ShowCardsGameAction(cards));
+                await SortCards(cards, null);
+                await TakeHorror(cards);
+            }
+        }
+
+        private async Task SortCards(IEnumerable<Card> cards, Owner owner)
+        {
+            Zone zoneToReturn = owner is Investigator investigator ? investigator.DeckZone : _chaptersProvider.CurrentScene.DangerDeckZone;
+            Card cardAffected = owner is Investigator investigator2 ? investigator2.InvestigatorCard : null;
+
+            InteractableGameAction interactableGameAction = new(canBackToThisInteractable: true, mustShowInCenter: true, "Choose card");
+            foreach (Card card in cards)
+            {
+                interactableGameAction.CreateEffect(card, new Stat(0, false), SelectCard, PlayActionType.Choose, ControlOwner, cardAffected: cardAffected);
+
+                async Task SelectCard()
+                {
+                    await _gameActionsProvider.Create(new MoveCardsGameAction(card, zoneToReturn, isFaceDown: true));
+                    IEnumerable<Card> newCards = cards.Except(new[] { card });
+                    if (newCards.Any()) await SortCards(newCards, owner);
+                }
+            }
+
+            await _gameActionsProvider.Create(interactableGameAction);
+        }
+
+
+        private async Task TakeHorror(IEnumerable<Card> cards)
+        {
+            if (!cards.Any(card => card.HasThisTag(Tag.Terror) || card.HasThisTag(Tag.Omen))) return;
+            await _gameActionsProvider.Create(new HarmToInvestigatorGameAction(ControlOwner, this, amountFear: 1));
+        }
     }
 }
--- Assets/SRC/Tests.PlayMode/Tests.PlayModeCORE1/Cards/CardSupply01690Tests.cs
+++ Assets/SRC/Tests.PlayMode/Tests.PlayModeCORE1/Cards/CardSupply01690Tests.cs
@@ -13,22 +13,37 @@ namespace MythosAndHorrors.PlayModeCORE1.Tests
         //protected override TestsType TestsType => TestsType.Debug;
 
         [UnityTest]
-        public IEnumerator TakeResources()
+        public IEnumerator SortCardWithFear()
         {
-            Assert.That(false, "Not Implemented");
             Investigator investigator = _investigatorsProvider.First;
-            Card01690 cardSupply = _cardsProvider.GetCard<Card01690>();
+            //Investigator investigator2 = _investigatorsProvider.Second;
+            yield return BuildCard("01690", investigator);
+            Card01690 supply = _cardsProvider.GetCard<Card01690>();
+
+            Card card1 = _cardsProvider.GetCard<Card01530>();
+            Card card2 = _cardsProvider.GetCard<Card01166>();
+            Card card3 = _cardsProvider.GetCard<Card01525>();
+
             yield return PlaceOnlyScene();
             yield return PlayThisInvestigator(investigator);
+            //yield return PlayThisInvestigator(investigator2);
 
-            yield return _gameActionsProvider.Create(new MoveCardsGameAction(cardSupply, investigator.HandZone)).AsCoroutine();
+            yield return _gameActionsProvider.Create(new MoveCardsGameAction(new[] { card1, card2, card3 }, SceneCORE1.DangerDeckZone, isFaceDown: true)).AsCoroutine();
+            yield return _gameActionsProvider.Create(new MoveCardsGameAction(supply, investigator.AidZone)).AsCoroutine();
 
-            Task gameActionTask = _gameActionsProvider.Create(new PlayInvestigatorGameAction(investigator));
-            yield return ClickedIn(cardSupply);
+            Task<PlayInvestigatorGameAction> taskGameAction = _gameActionsProvider.Create(new PlayInvestigatorGameAction(investigator));
+            yield return ClickedIn(supply);
+            yield return ClickedIn(SceneCORE1.CardDangerToDraw);
+            yield return ClickedIn(card3);
+            yield return ClickedIn(card1);
+            //yield return ClickedIn(card2);
+            Assert.That(investigator.CurrentTurns.Value, Is.EqualTo(3));
             yield return ClickedMainButton();
-            yield return gameActionTask.AsCoroutine();
+            yield return taskGameAction.AsCoroutine();
 
-            Assert.That(investigator.Resources.Value, Is.EqualTo(8));
+            Assert.That(supply.Charge.Amount.Value, Is.EqualTo(2));
+            Assert.That(SceneCORE1.CardDangerToDraw, Is.EqualTo(card2));
+            Assert.That(investigator.FearRecived.Value, Is.EqualTo(1));
         }
     }
 }
