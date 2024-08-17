7395167f28984b427a95a677e73e5b3ef23380fc
Lucas Rosales <yonk_cs@hotmail.com>
2024-08-17 10:14:51 +0200

    Initial Settings

---
+++ Assets/SRC/GameView/Configs/InitialSettings.cs
@@ -0,0 +1,40 @@
+﻿using MythosAndHorrors.GameRules;
+using System;
+using System.Threading.Tasks;
+using UnityEngine;
+using Zenject;
+
+namespace MythosAndHorrors.GameView
+{
+    public class InitialSettings : MonoBehaviour
+    {
+        [Inject] private readonly ReactionablesProvider _reactionablesProvider;
+        [SerializeField] private bool _withoutAudio;
+        [SerializeField] private bool _withoutMulligan;
+        [SerializeField, Range(1, 64)] private float _gameVelocity = 1;
+        [SerializeField] private Languaje _languaje;
+
+        /*******************************************************************/
+        private void Awake()
+        {
+            if (_withoutAudio)
+            {
+                AudioListener.volume = 0;
+            }
+            if (_withoutMulligan)
+            {
+                _reactionablesProvider.CreateReaction<MulliganGameAction>(CancelMulliganCondition, CancelMulligan, GameActionTime.Before);
+
+                async Task CancelMulligan(MulliganGameAction mulliganGameAction)
+                {
+                    mulliganGameAction.Cancel();
+                    await Task.CompletedTask;
+                }
+
+                bool CancelMulliganCondition(MulliganGameAction _) => true;
+            }
+
+            Time.timeScale = _gameVelocity;
+        }
+    }
+}
