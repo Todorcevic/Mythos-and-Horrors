87e579e5a4fe1617ccc910da38b0ebf291b6761e
Lucas Rosales <yonk_cs@hotmail.com>
2024-11-28 17:26:54 +0100

    Audio move

--- Assets/SRC/GameView/Views/Cards/CardView.cs
+++ Assets/SRC/GameView/Views/Cards/CardView.cs
@@ -125,7 +125,7 @@ namespace MythosAndHorrors.GameView
         public Tween MoveToZone(ZoneView newZoneView, Ease ease = Ease.InOutCubic)
         {
             if (newZoneView == CurrentZoneView) return DOTween.Sequence();
-            ;
+            
             Sequence moveSequence = DOTween.Sequence().SetId(ViewValues.MOVE_ANIMATION)
                  .OnPlay(() => _audioComponent.PlayAudio(_moveAudio))
                  .OnStart(() => transform.SetParent(newZoneView.transform))
