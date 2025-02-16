4e275701bbdcd43bd83370e5100e5ae009d0139e
Lucas Rosales <yonk_cs@hotmail.com>
2025-02-16 15:16:21 +0100

    investigator swap audio

--- Assets/SRC/GameView/Components/SwapInvestigatorComponent.cs
+++ Assets/SRC/GameView/Components/SwapInvestigatorComponent.cs
@@ -11,22 +11,24 @@ namespace MythosAndHorrors.GameView
     {
         private AreaInvestigatorView _currentAreaInvestigator;
         [Inject] private readonly AreaInvestigatorViewsManager _areaInvestigatorViewsManager;
+        [Inject] private readonly AudioComponent _audioComponent;
         [SerializeField, Required, ChildGameObjectsOnly] private Transform _playPosition;
         [SerializeField, Required, ChildGameObjectsOnly] private Transform _rightPosition;
         [SerializeField, Required, ChildGameObjectsOnly] private Transform _leftPosition;
         [SerializeField, Required, ChildGameObjectsOnly] private Transform _optimizationPosition;
+        [SerializeField, Required, AssetsOnly] private AudioClip _swapAudio;
+
 
         /*******************************************************************/
         public Tween Select(Investigator investigator)
         {
             AreaInvestigatorView areaInvestigatorView = _areaInvestigatorViewsManager.Get(investigator);
             (Transform positionToExit, Transform positionToEnter) = GetSidePosition(investigator);
-            return DOTween.Sequence().OnStart(() => areaInvestigatorView.transform.position = positionToEnter.position)
+            return DOTween.Sequence().OnStart(() => { areaInvestigatorView.transform.position = positionToEnter.position; _audioComponent.PlayAudio(_swapAudio); })
                 .Append(areaInvestigatorView.transform.DOFullMove(_playPosition, ViewValues.MID_TIME_ANIMATION).SetEase(Ease.InOutQuad))
-                .Join(_currentAreaInvestigator == null ?
-                DOTween.Sequence() :
-                _currentAreaInvestigator.transform.DOFullMove(positionToExit, ViewValues.MID_TIME_ANIMATION).SetEase(Ease.InOutQuad))
-                .OnComplete(() => _currentAreaInvestigator = areaInvestigatorView);
+                .Join(_currentAreaInvestigator == null ? DOTween.Sequence() :
+                    _currentAreaInvestigator.transform.DOFullMove(positionToExit, ViewValues.MID_TIME_ANIMATION).SetEase(Ease.InOutQuad))
+                    .OnComplete(() => _currentAreaInvestigator = areaInvestigatorView);
         }
 
         private (Transform, Transform) GetSidePosition(Investigator investigator) =>
